c7ba0c367c2c17014a6b0fa010ab4d2d7314d739
Karine FOCK LAW <kflmada@gmail.com>
2020-02-07 16:24:04 +0100

    TEST COLISION

--- js/pacman.js
+++ js/pacman.js
@@ -20,19 +20,20 @@ var grille=[
 [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],//  19
 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],//  20
 ];
+var _grille= document.getElementById('grille');
+
 function initGrille(){
     /*vider la grille*/
-    let _grille= document.getElementById("grille");
     _grille.innerHTML="";
     /* dimension dynamique de la grille*/
     _grille.style.gridTemplateColumns="repeat(19, 40px)";
     _grille.style.gridTemplateRows="repeat(22, 40px)";
-    console.log(grille)
-
+  //  console.log(grille)
+  //  console.log(pacman)
 for(let ligne in grille){
     for(let col in grille [ligne]){
-        console.log(grille[ligne][col])
-        let monelement=document.createElement("div")
+    //    console.log(grille[ligne][col])
+        let monelement=document.createElement("div");
         if ((grille[ligne][col]) ==(0)){
             monelement.classList.add("mur");
         }
@@ -42,10 +43,68 @@ for(let ligne in grille){
         else     if ((grille[ligne][col])==(2)){
             monelement.classList.add("bonbon");
         }
+        monelement.style.gridColumn=(+col)+1;
+        monelement.style.gridRow=(+ligne)+1;
         _grille.appendChild(monelement);
     }
 
-console.log(grille[ligne]);
+//console.log(grille[ligne])
+
 }
 }
-initGrille();
+
+function boucleRefresh(){
+    initGrille();
+    console.log("reflesh");
+    bougePacman();
+    setTimeout(boucleRefresh, 1000);
+}
+var pacman = {
+    x : 5,
+    y : 2,
+    direction : 0
+};
+function bougePacman(){
+
+    if(pacman.direction==0){
+        pacman.x ++;
+    }
+    else if(pacman.direction==1){
+        pacman.y ++;
+    }
+    else if(pacman.direction==2){
+        pacman.x --;
+    }
+    else if(pacman.direction==3){
+        pacman.y --;
+    }
+    var pacmanElem=document.createElement("div")
+    pacmanElem.classList.add("pacman");
+    pacmanElem.style.gridColumn=(pacman.x);
+    pacmanElem.style.gridRow=(pacman.y);
+    _grille.appendChild(pacmanElem);
+}
+boucleRefresh();
+document.onkeypress=appuieTouche;
+function appuieTouche(event){
+    console.log(event.key);
+    switch(event.key){
+        case"c":
+        case"C":
+        pacman.direction=1
+        break;
+        case"s":
+        case"S":
+        pacman.direction=2
+        break;
+        case"e":
+        case"ETEST":
+        pacman.direction=3
+        break;
+        case"f":
+        case"F":
+        pacman.direction=0
+        break;
+    }
+
+}
